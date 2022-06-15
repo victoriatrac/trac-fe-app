512274eae314eda913c85bdf24fbee9410010cbf
Victoria Trac <victoriatrac@gmail.com>
2022-06-14 21:18:22 -0600

    added arbitrary second component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Weather from './components/Weather'
 import './App.css'
+import Weather from './components/Weather'
+import Mock2 from './components/Mock2'
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Mock2 />
     </div>
   );
 }
